fd6d19b4acf3d8cc61065eab1298959cf2a33acf
Jashitha Rashmitha <123060642+Jashi111@users.noreply.github.com>
2024-10-25 21:38:21 +0530

    SampleProject

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form.get('name')
+    if not name:
+        return redirect(url_for('index'))
+    return render_template('greet.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
